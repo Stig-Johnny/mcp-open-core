f238838b250bdde6161b1215c184be6dcd684eb5
Stig-Johnny <saintjon75@hotmail.com>
2025-06-12 10:28:40 +0200

    Create news_parser.py

---
+++ data_ingestion/news_parser.py
@@ -0,0 +1,17 @@
+# data_ingestion/news_parser.py
+
+import feedparser
+
+class NewsParser:
+    def __init__(self, rss_url="https://cryptonews.com/news/feed"):
+        self.rss_url = rss_url
+
+    def fetch_headlines(self):
+        feed = feedparser.parse(self.rss_url)
+        articles = [{"title": entry.title, "link": entry.link} for entry in feed.entries]
+        return articles
+
+if __name__ == "__main__":
+    np = NewsParser()
+    for article in np.fetch_headlines():
+        print(article)
